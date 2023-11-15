184cec92bab57f1a3f6c32c6d59f5b0c136a5f63
sjgmajor <sjg3234@gmail.com>
2023-11-15 13:34:20 +0900

    commit

--- msa-practices/clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
+++ msa-practices/clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.poscodx.emaillist.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import com.poscodx.emaillist.dto.JsonResult;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public String NoHandlerFoundException(Exception e) {
+		// 응답
+		return "index";
+	}
+	
 	@ResponseBody
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<JsonResult> handlerException(Exception e) {
+	public ResponseEntity<?> handlerException(Exception e) {
 		
 		// 로깅(Logging)
 		StringWriter errors = new StringWriter();
@@ -27,13 +33,8 @@ public class GlobalExceptionHandler {
 		log.error(errors.toString());
 
 		// 응답
-		JsonResult jsonResult =
-				(e instanceof NoHandlerFoundException) ?
-						JsonResult.fail("Unknown Request") :
-						JsonResult.fail(errors.toString());
-		
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(jsonResult);
+				.body(Map.of("error", errors.toString()));
 	}
 }
--- msa-practices/monolithics/mysite07/backend/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ msa-practices/monolithics/mysite07/backend/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -1,30 +1,37 @@
-package com.poscodx.msa.service.emaillist.exception;
+package com.poscodx.mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import com.poscodx.mysite.dto.JsonResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.poscodx.msa.service.emaillist.dto.JsonResult;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-@Slf4j
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-	@ResponseBody
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public String handlerNoHandlerFoundException(Exception e) {
+		return "index";
+	}
+
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handlerException(Exception e) {		
-		// 로깅(Logging)
+	@ResponseBody
+	public ResponseEntity<JsonResult> handlerException(Exception e) {
+		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
-
-		// 응답
+		
+		// 2. JSON 응답
 		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.fail(errors.toString()));
 	}
 }
+
